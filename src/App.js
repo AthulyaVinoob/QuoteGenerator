c8fa2b2534ab514b668e22f562804e37c45a5de0
Athulya <athulya.v.96@gmail.com>
2019-12-02 21:25:34 +0530

    Quote Generator tagged to button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { random } from 'lodash';
 import './App.css';
-import Button from './component/Button';
+import QuoteComponent from './component/QuoteComponent';
 
 class App extends Component {
   constructor(props) {
@@ -11,23 +11,26 @@ class App extends Component {
       selectedQuoteIndex: null,
     }
     this.selectQuoteIndex = this.selectQuoteIndex.bind(this);
+    this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
   }
 
   componentDidMount() {
     fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
       .then(data => data.json())
-      .then(quotes => this.setState({ quotes }, () => {
-        this.setState({ selectedQuoteIndex: this.selectQuoteIndex() })
-      }));
+      .then(quotes => this.setState({ quotes }, this.assignNewQuoteIndex));
   }
-
+/*
+Selects the new quote
+*/
   get selectedQuote() {
     if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
-      return;
+      return undefined;
     }
     return this.state.quotes[this.state.selectedQuoteIndex];
   }
-
+/*
+generates random index for quote
+*/
   selectQuoteIndex() {
     if (!this.state.quotes.length) {
       return;
@@ -35,11 +38,19 @@ class App extends Component {
     return random(0, this.state.quotes.length - 1);
   }
 
+/*
+Updates state by passing new index
+*/
+  assignNewQuoteIndex(){
+    this.setState({selectedQuoteIndex : this.selectQuoteIndex()});
+  }
+
   render() {
     return (
       <div className="App" id="quote-box">
-        { this.selectedQuote ? `"${this.selectedQuote.quote}" - ${this.selectedQuote.author}` : '' }
-        <Button buttonText="Next Quote" clickHandler={this.nextQuoteClickHandler} />
+        <QuoteComponent selectedQuote={this.selectedQuote}
+                        assignNewQuoteIndex ={this.assignNewQuoteIndex}
+        />
       </div>
     );
   }
